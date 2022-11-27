ad4972e140b2bb98af910080dbdac4c9002314a1
Anoshko Alexey <anoshkoalexey@gmail.com>
2022-11-27 13:48:43 -0800

    Add mappers

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -1,25 +1,33 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface CredentialMapper {
-    @Select("SELECT * FROM credentials WHERE userid = #{userId}")
-    List<Credential> getCredentials(int userId);
 
     @Insert("INSERT INTO credentials (url, username, key, password, userid) VALUES(#{url}, #{userName}, #{key}, #{password}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "credentialId")
     int insert(Credential credential);
 
+    @Select("SELECT * FROM credentials WHERE credentialid = #{credentialid} AND userid = #{userId}")
+    Credential select(Credential credential);
+
+    @Select("SELECT * FROM credentials WHERE credentialid = #{credentialid}")
+    Credential selectById(Integer credentialId);
+
+    @Select("SELECT * FROM credentials WHERE userid = #{userid}")
+    List<Credential> selectByUser(User user);
+
+    @Select("SELECT * FROM credentials WHERE userid = #{userId}")
+    List<Credential> selectByUserId(Integer userId);
+
     @Update("UPDATE credentials SET url = #{url}, username = #{username}, key = #{key}, password= #{password}, userid = #{userid} WHERE credentialId = #{credentialId}")
-    void updateCredential(Credential credential);
+    int update(Credential credential);
 
-    @Delete("DELETE FROM credentials WHERE credentialid = #{credentialId}")
-    void deleteCredential(int credentialId);
-
-    @Select("SELECT * FROM credentials WHERE credentialid = #{credentialId}")
-    Credential getCredentialById(int credentialId);
+    @Delete("DELETE FROM credentials WHERE credentialid = #{credentialId} and userid = #{userid}")
+    int delete(Integer credentialid, Integer userid);
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,22 +1,33 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface NoteMapper {
-    @Select("SELECT * FROM notes WHERE userid = #{userId}")
-    Note[] getNotesByUserId(Integer userId);
 
     @Insert("INSERT INTO notes (notetitle,  notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
-    int insert(Note note);
+    Integer insert(Note note);
 
-    @Delete("DELETE FROM notes WHERE noteid = #{noteId}")
-    void deleteNote(int noteId);
+    @Select("SELECT * FROM notes WHERE noteid = #{noteId} AND userid = #{userId}")
+    Note selectById(Integer noteId, Integer userid);
 
     @Select("SELECT * FROM notes WHERE noteid = #{noteId}")
-    Note getNoteById(int noteId);
+    List<Note> select(Note note);
+
+    @Select("SELECT * FROM notes WHERE userid = #{userId}")
+    List<Note> selectByUserId(Integer userId);
+
+    @Select("SELECT * FROM notes WHERE userid = #{userId}")
+    List<Note> selectByUser(User user);
 
     @Update("UPDATE notes SET notetitle = #{noteTitle}, notedescription = #{notedescription} WHERE noteid = #{noteId}")
-    void updateNote(Note note);
+    int updateNote(Note note);
+
+    @Delete("DELETE FROM notes WHERE noteid = #{noteId} AND userid = #{userId}")
+    int delete(Integer noteId, Integer userId);
+
 }
