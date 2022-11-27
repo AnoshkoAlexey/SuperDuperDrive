fc9f25f95482672313f2415331ee757c47b7a8ac
Anoshko Alexey <anoshkoalexey@gmail.com>
2022-11-27 13:51:28 -0800

    Edit models

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -1,14 +1,14 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class File {
-    private int fileId;
+    private Integer fileId;
     private String fileName;
     private String contentType;
     private String fileSize;
-    private int userId;
+    private Integer userId;
     private byte[] fileData;
 
-    public File(int fileId, String fileName, String contentType, String fileSize, Integer userId, byte[] fileData) {
+    public File(Integer fileId, String fileName, String contentType, String fileSize, Integer userId, byte[] fileData) {
         this.fileId = fileId;
         this.fileName = fileName;
         this.contentType = contentType;
@@ -17,9 +17,9 @@ public class File {
         this.fileData = fileData;
     }
 
-    public int getFileId() { return fileId; }
+    public Integer getFileId() { return fileId; }
 
-    public void setFileId(int fileId) { this.fileId = fileId; }
+    public void setFileId(Integer fileId) { this.fileId = fileId; }
 
     public String getFileName() { return fileName; }
 
@@ -41,11 +41,11 @@ public class File {
         this.fileSize = fileSize;
     }
 
-    public int getUserid() {
+    public Integer getUserid() {
         return userId;
     }
 
-    public void setUserId(int userId) {
+    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -7,16 +7,16 @@ package com.udacity.jwdnd.course1.cloudstorage.model;
  */
 public class User {
 
-    private int userId;
-    private String userName;
+    private Integer userId;
+    private String username;
     private String salt;
     private String password;
     private String firstName;
     private String lastName;
 
-    public User(int userId, String userName, String salt, String password, String firstName, String lastName) {
+    public User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
         this.userId = userId;
-        this.userName = userName;
+        this.username = username;
         this.salt = salt;
         this.password = password;
         this.firstName = firstName;
@@ -27,16 +27,16 @@ public class User {
         return userId;
     }
 
-    public void setUserId(int userId) {
+    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String userName) {
+        this.username = userName;
     }
 
     public String getSalt() {
